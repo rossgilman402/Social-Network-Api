5ef5eea7567f4afb3cd69d467a18cdfcc51dcf03
ross402 <65184393+ross402@users.noreply.github.com>
2023-10-17 20:26:44 -0400

    Added seeding to users and thoughts

---
+++ utils/seed.js
@@ -0,0 +1,55 @@
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const thoughtData = {
+  thoughtText: "Here's a cool thought...",
+  username: "lernantino",
+  userId: "652f25bcaf53305bb6dba104",
+};
+
+const userData = [
+  {
+    username: "lernantino",
+    email: "lernantino@gmail.com",
+  },
+  {
+    username: "sauce",
+    email: "sauce@gmail.com",
+  },
+  {
+    username: "butter",
+    email: "butter@gmail.com",
+  },
+  {
+    username: "ross",
+    email: "ross@gmail.com",
+  },
+];
+
+// Start the seeding runtime timer
+console.time("seeding");
+
+//Create connection to mongoDB
+connection.once("open", async () => {
+  //Delete collections if they exist
+  let userCheck = await connection.db
+    .listCollections({ name: "Users" })
+    .toArray();
+  if (userCheck.length) {
+    await connection.dropCollection("Users");
+  }
+
+  let thoughtCheck = await connection.db
+    .listCollections({ name: "Thoughts" })
+    .toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection("Thoughts");
+  }
+
+  await User.collection.insertMany(userData);
+  await Thought.collection.insertOne(thoughtData);
+
+  console.table(userData);
+  console.table(thoughtData);
+  console.timeEnd("Seeding Complete");
+  process.exit(0);
+});
