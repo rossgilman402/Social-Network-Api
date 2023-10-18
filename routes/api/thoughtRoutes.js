43293d5a5497cb5583253da1d60dddb02e9ae46c
ross402 <65184393+ross402@users.noreply.github.com>
2023-10-18 19:50:04 -0400

    Added to the thought routes with the thoughtController methods

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,2 +1,22 @@
 const router = require("express").Router();
-const {} = require("../controllers/userThoughts.js");
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  //   addFriend,
+  //   deleteFriend,
+} = require("../../controllers/thoughtController.js");
+
+//api/thoughts/
+router.route("/").get(getThoughts).post(createThought);
+
+//api/thoughts/:userId
+router
+  .route("/:userId")
+  .get(getSingleThought)
+  .delete(deleteThought)
+  .put(updateThought);
+
+module.exports = router;
